fa60df5e09a7d14b5fc6da2ca416929e9feaabcd
RohanRamola <rohanramola63@gmail.com>
2023-05-23 23:33:43 +0530

    Initial commit

---
+++ app/src/main/java/com/delivery/quickie/home_page.java
@@ -0,0 +1,28 @@
+package com.delivery.quickie;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.delivery.quickie.databinding.ActivityHomePageBinding;
+import com.delivery.quickie.databinding.ActivityMainBinding;
+
+public class home_page extends AppCompatActivity {
+
+    ActivityHomePageBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_home_page);
+        binding.press.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(home_page.this, "pressed", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
